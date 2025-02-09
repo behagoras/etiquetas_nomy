cf6a081294034b12f3707e4111f19eae191e00e7
David Behar <davidbehar@Davids-MacBook-Pro-2.local>
2025-02-08 23:40:39 -0600

    Split trays in chunks for the first page

--- src/firstPage.ts
+++ src/firstPage.ts
@@ -2,26 +2,35 @@ import * as XLSX from 'xlsx';
 import { getIconFromType } from "./icons";
 import { FirstPAge, Groups } from "./types";
 
-
 export const getFirstPage = (workbook: XLSX.WorkBook) => {
   const firstSheet = workbook.Sheets[workbook.SheetNames[0]];
   const firstSheetData = XLSX.utils.sheet_to_json<FirstPAge>(firstSheet);
 
+  const firstPage: Groups = {};
 
-  const firstPage: Groups = firstSheetData.reduce((acc: Groups, row: FirstPAge) => {
+  firstSheetData.forEach((row: FirstPAge) => {
     const { type, tray } = row;
 
-    // Initialize group if it doesn't exist
-    const group = acc[type] || (acc[type] = {});
+    if (!firstPage[type]) firstPage[type] = {};
+    if (!firstPage[type][tray]) firstPage[type][tray] = [];
 
-    // Initialize tray array if it doesn't exist
-    const trayArray = group[tray] || (group[tray] = []);
+    const rowWithIcon = { ...row, icon: getIconFromType(type) };
+    firstPage[type][tray].push(rowWithIcon);
+  });
 
-    // Add row to the tray array
-    trayArray.push({ ...row, icon: getIconFromType(type) });
 
-    return acc;
-  }, {});
+  const chunkPages = Object.entries(firstPage).map(([type, trays]) => {
+    return {
+      [type]: Object.entries(trays).reduce((acc: { [tray: string]: FirstPAge[][] }, [tray, items]) => {
+        acc[tray] = [];
+        for (let i = 0; i < items.length; i += 11) {
+          acc[tray].push(items.slice(i, i + 11));
+        }
+        return acc;
+      }, {})
+    };
+  });
 
-  return firstPage;
-}
+
+  return chunkPages;
+};
